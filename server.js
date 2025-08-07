4a14415421cc8f9f227eb2be485a9986f1c336ef
eroson24 <erolsonmeznj@gmail.com>
2025-08-06 20:11:51 -0400

    about page style changes

--- server.js
+++ server.js
@@ -239,7 +239,7 @@ app.get("/api/cooldown-time", async function (req, res) {
 
         const timeLeftSeconds = await redisClient.ttl(cooldownKey);
 
-        // Redis TTL returns -2 if the key doesn't exist, -1 if it exists but has no expiration,
+        // Redis TTL returns -2 if await redisClient.setEx(cooldownKey, cooldownPeriodSeconds, "on");the key doesn't exist, -1 if it exists but has no expiration,
         // and a positive number for the seconds remaining. Treat -2 as 0.
         const timeRemaining = timeLeftSeconds > 0 ? timeLeftSeconds : 0;
 
