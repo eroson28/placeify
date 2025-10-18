2ec1d5ce1a5c19140162b866b3a1e4d1e948da84
eroson24 <erolsonmeznj@gmail.com>
2025-10-18 12:52:52 -0400

    Reverted Grid changes

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,22 +1,6 @@
 import { useState, useEffect } from "react";
 import Tile from "./Tile";
 
-const mapDatabaseKeys = (dbTile) => {
-    if (!dbTile) return null;
-    return {
-        rowNum: dbTile.rownum,
-        colNum: dbTile.colnum,
-        songName: dbTile.songname || null,
-        artistName: dbTile.artistname || null,
-        coverArtUrl: dbTile.coverarturl || null,
-        isSelected: false,
-        username: dbTile.username || null,
-        lastUpdated: dbTile.lastupdated,
-        spotifyLink: dbTile.link || null,
-        albumName: dbTile.albumname || null,
-    };
-};
-
 function Grid() {
   const [gridData, setGridData] = useState(() => {
     const initialGrid = [];
@@ -89,8 +73,7 @@ function Grid() {
 
       const fetchedTilesMap = new Map();
       fetchedData.forEach((tile) => {
-        const mappedTile = mapDatabaseKeys(tile);
-        fetchedTilesMap.set(`${mappedTile.rowNum}-${mappedTile.colNum}`, mappedTile);
+        fetchedTilesMap.set(`${tile.rowNum}-${tile.colNum}`, tile);
       });
 
       const updatedGrid = [];
