272872add6df9e2af78500327507c228f483ab83
eroson24 <sonmez.12@osu.edu>
2025-10-19 17:49:34 -0400

    Grid alert change

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -33,11 +33,7 @@ function Grid() {
     });
 
     const handleOpenGenericModal = (message, isError = false) => {
-        setGenericModal({
-            isOpen: true,
-            message,
-            isError,
-        });
+        alert(message);
     };
 
   // Function to fetch the current cooldown time from the server
@@ -201,37 +197,6 @@ function Grid() {
           );
         })}
       </div>
-
-      {genericModal.isOpen && (
-      <div
-        className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
-        onClick={() =>
-          setGenericModal({ isOpen: false, message: "", isError: false })
-        }
-      >
-        <div
-          className="bg-white p-6 rounded-lg shadow-lg max-w-sm w-full text-center"
-          onClick={(e) => e.stopPropagation()}
-        >
-          <p
-            className={`font-semibold ${
-              genericModal.isError ? "text-red-600" : "text-green-600"
-            }`}
-          >
-            {genericModal.message}
-          </p>
-          <button
-            className="mt-4 px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600"
-            onClick={() =>
-              setGenericModal({ isOpen: false, message: "", isError: false })
-            }
-          >
-            OK
-          </button>
-        </div>
-      </div>
-    )}
-    
     </div>
   );
 }
